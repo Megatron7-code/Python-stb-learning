9a16f27691befd31034c3a7cc84ee63547192e90
megatron <1251783361@qq.com>
2019-08-27 13:57:47 +0800

    adjust

--- src/network/tcp_sisteen.py
+++ src/network/tcp_sisteen.py
@@ -11,15 +11,15 @@ def recvall(sock, length):
 
 def server(interface, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # 这里参数表明使用操作系统还保留着的准备断开的套接字地址。不然会提示端口已占用
     sock.bind((interface, port))
-    sock.listen(1)
+    sock.listen(1) # 这里的1指明了处于等待的连接的最大数目, 如果还未创建会被压入栈中。超过数目会被抛掉
     print('Listening at', sock.getsockname())
     while True:
-        sc, sockname = sock.accept()
+        sc, sockname = sock.accept() # 为连接创建套接字
         print('We have accepted a connection from', sockname)
         print(' Socket name:', sc.getsockname())
-        print(' Socket peer:', sc.getpeername())
+        print(' Socket peer:', sc.getpeername()) # 获取客户端的地址
         message = recvall(sc, 16)
         print(' Incoming sixteen-octet message:', repr(message))
         sc.sendall(b'Farewell, client')
