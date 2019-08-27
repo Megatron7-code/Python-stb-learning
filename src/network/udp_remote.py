50079711efe50a2c041b558af2ab1f18401d4809
megatron <1251783361@qq.com>
2019-08-27 11:27:28 +0800

    adjust

--- src/network/udp_remote.py
+++ src/network/udp_remote.py
@@ -7,7 +7,7 @@ def server(interface, port):
     sock.bind((interface, port))
     print('Listening at', sock.getsockname())
     while True:
-        data, address = sock.getsockname()
+        data, address = sock.recvfrom(MAX_BYTES)
         if random.random() < 0.5:
             print('Pretending to drop packet from {}'.format(address))
             continue
@@ -22,3 +22,30 @@ def client(hostname, port):
     hostname = sys.argv[2]
     sock.connect((hostname, port))
     print('Client socket name is {}'.format(sock.getsockname()))
+    delay = 0.1 # seconds
+    text = 'This is another message'
+    data = text.encode('ascii')
+    while True:
+        sock.send(data)
+        print('Waiting up to {} seconds for a reply'.format(delay))
+        sock.settimeout(delay)
+        try:
+            data = sock.recv(MAX_BYTES)
+        except socket.timeout:
+            delay *= 2
+            if delay > 2.0:
+                raise RuntimeError('I think the server is down')
+        else:
+            break
+    print('The server says {!r}.'.format(data.decode('ascii')))
+
+
+if __name__ == '__main__':
+    choices = {'client': client, 'server': server}
+    parser = argparse.ArgumentParser(description='Send and receive UDP, preending packets are often dropped')
+    parser.add_argument('role', choices=choices, help='which role to take')
+    parser.add_argument('host', help='interface the server listens at; host the client sends to')
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060, help='UDP port (default 1060)')
+    args = parser.parse_args()
+    function = choices[args.role]
+    function(args.host, args.p)
